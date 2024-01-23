da9c2f4b100eb045d8531ad62df50b480de3359a
Marcelo Valois <mmv147.valois@gmail.com>
2024-01-23 17:49:24 -0300

    Ajustando configuração do socket e adicionando snippets

--- src/sockets/socketManager.ts
+++ src/sockets/socketManager.ts
@@ -8,35 +8,44 @@ class SocketManager {
 
   private handleSocketConnection(): void {
     this.io.on("connection", async (socket: Socket) => {
-      const id = socket.handshake.query.id;
-      const parsedId = Number(id);
+      try {
+        const id = socket.handshake.query.id;
+        if (!id) {
+          socket.disconnect();
+          return;
+        }
 
-      const user = await findUserByIdUseCase.execute(parsedId);
-      console.log(user);
+        const parsedId = Number(id);
 
-      if (user) {
-        socket.join(user.id?.toString() || "");
-        console.log(`User ${user.id} connected to socket id ${socket.id}`);
-      } else {
-        socket.disconnect();
+        const user = await findUserByIdUseCase.execute(parsedId);
+        console.log(user);
+
+        if (user) {
+          socket.join(user.id?.toString() || "");
+          console.log(`User ${user.id} connected to socket id ${socket.id}`);
+        } else {
+          socket.disconnect();
+        }
+
+        socket.emit("getId", socket.id);
+
+        socket.on("sendMessageToServer", async (data: PontuandoMessage) => {
+          socket.broadcast.emit("sendMessageToClient", data);
+        });
+
+        socket.on("sendMessageToChatServer", async (data: PontuandoMessage) => {
+          socket.broadcast.emit("sendMessageToChatClient", data);
+        });
+
+        socket.on("disconnect", () => {
+          // let users: UserData[] = myCache.get("users") || [];
+          // users = users.filter((user) => user.id !== socket.id);
+          // myCache.set("users", users);
+          // socket.broadcast.emit("removeContactFromClient", socket.id);
+        });
+      } catch (error) {
+        console.error(`Error handling socket connection: ${error}`);
       }
-
-      socket.emit("getId", socket.id);
-
-      socket.on("sendMessageToServer", async (data: PontuandoMessage) => {
-        socket.broadcast.emit("sendMessageToClient", data);
-      });
-
-      socket.on("sendMessageToChatServer", async (data: PontuandoMessage) => {
-        socket.broadcast.emit("sendMessageToChatClient", data);
-      });
-
-      socket.on("disconnect", () => {
-        // let users: UserData[] = myCache.get("users") || [];
-        // users = users.filter((user) => user.id !== socket.id);
-        // myCache.set("users", users);
-        // socket.broadcast.emit("removeContactFromClient", socket.id);
-      });
     });
   }
 }
